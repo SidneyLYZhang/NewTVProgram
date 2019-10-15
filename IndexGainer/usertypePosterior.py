1e2ccfd684e29aad87614676232a1f0c55b6e6d5
SidneyZhang <liangyi@me.com>
2019-10-15 17:57:50 +0800

    plus

--- IndexGainer/usertypePosterior.py
+++ IndexGainer/usertypePosterior.py
@@ -45,17 +45,115 @@ import json
 import pandas as pd
 import time
 import random
+import psutil
+import numpy as np
+from pympler.asizeof import asizeof
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KernelDensity
 ## own packages
 from indexGainer import indexGainer as indG
 
 # CLASS
 
+class effectActions(object):
+    '''
+    计算用户的收视行为中哪些行为可以算作有效行为。
+    输入以'pandas.DataFrame'类型作为输入。
+    '''
+    def __init__(self, actions, min_max_range = (0, 60), content = None):
+        if not content :
+            self.__content_name = actions.columns.tolist()[0]
+        elif content in actions.columns.tolist() :
+            self.__content_name = content
+        else :
+            print('Selected a unvalid column name, content column name will be used by default.')
+            self.__content_name = actions.columns.tolist()[0]
+        self.__O_data = actions
+        self.__transfit_data = self.__transfit(min_max_range)
+        self.upshot = self.__load()
+    
+    def __transfit(self, min_max_range):
+        boundary = list(map(lambda x : max(self.__O_data[x]) - min(self.__O_data[x]) , self.__O_data))
+        boundary = dict(zip(
+            self.__O_data.columns.tolist(),
+            list(map(lambda x : int(np.ceil(min_max_range[1] * x / max(boundary))), boundary))
+        ))
+        tmp = []
+        res = self.__O_data.copy()
+        for i in res:
+            res = res[(res[i] != 0 )]
+        for key,data in self.__O_data.items():
+            if max(data) >= 3600 :
+                tmp.extend([key])
+                res[key] = pd.DataFrame(
+                    MinMaxScaler(feature_range = (min_max_range[0] , boundary[key])).fit_transform(self.__O_data),
+                    columns = self.__O_data.columns.tolist()
+                )[key]
+            else:
+                res[key] = self.__O_data[key]
+        return res
+    
+    def __load(self):
+        kde = KernelDensity(kernel='gaussian', bandwidth = np.std(np.std(self.__transfit).tolist())).fit(self.__transfit)
+        kde = kde.score_samples(self.__transfit)
+        ok_kde = kde[kde > np.mean(kde)].tolist()
+        res = self.__O_data[(pd.DataFrame(kde).isin(ok_kde))][self.__content_name].unique().tolist()
+        return res
+    
+    def primitiveData(self):
+        return self.__O_data
+    
+    def effectWatchtime(self):
+        other = self.__O_data.columns.tolist().remove(self.__content_name)
+        other = list(map(lambda x : (x,min(self.__O_data[x])) , other))
+        s_min_col = 0
+        for i in other:
+            if i[1] > s_min_col:
+                s_min_col = i[1]
+                res = i[0]
+        res = self.__O_data[(self.__O_data[self.__content_name].isin(self.upshot))][res].min()
+        return res
+    
+    def effectRating(self, formatted = False):
+        res = len(self.upshot)/len(self.__O_data)
+        if formatted:
+            res = "{3.2f}%".format(res*100)
+        return res
+    
+    def RAM_info(self, output = False, virtual_memory = False):
+        data_size = asizeof(self.upshot)
+        data_size += asizeof(self.__O_data)
+        data_size += asizeof(self.__effectWatchtime)
+        data_size = data_size / 1024 / 1024
+        if output:
+            return (data_size, psutil.virtual_memory().percent) if virtual_memory else data_size
+        else :
+            print('This object has occupied {.2}MB of memory.'.format(data_size))
+            if virtual_memory :
+                print('Memory has been used {.2f}% of total.'.format(psutil.virtual_memory().percent))
+
+class beyesProbability(object):
+    '''
+    计算多条件贝叶斯概率（已知a在s_1至s_n可能有c属性的概率，求a有c的概率。）
+    '''
+    def __init__(self):
+        pass
+
 class bayesclassVerification(object):
     '''
     计算某用户在某互斥属性组下有效属性及对应概率。
     '''
-    def __init__(self, user_actions, probabilities, action_list = None):
-        pass
+    def __init__(self, user_actions, probabilities, action_list = None, content_name = None):
+        if content_name & content_name in user_actions.columns :
+            tip_content = content_name
+        else :
+            tip_content = user_actions.columns.tolist()[0]
+        if action_list :
+            peractions = user_actions[[tip_content] + action_list]
+        else :
+            peractions = user_actions
+        self.__effect_contents = effectActions(peractions).upshot
+        self.__probabilities = probabilities
     
     def __str__(self):
         pass
